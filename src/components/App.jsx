16205bf243fa2ffe7c575acc30fc69ccf72baef9
SvetaSaletska <svitlanagalevich@gmail.com>
2023-12-18 21:18:13 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,16 @@
-import { Profile } from "./Profile/Profile";
-
+import { Profile } from 'components/Profile/Profile';
+import user from './data/user.json';
 
 export const App = () => {
   return (
     <div>
-       <Profile />
+       <Profile
+         username={user.username}
+         tag={user.tag}
+         location={user.location}
+         avatar={user.avatar}
+         stats={user.stats}
+/>
     </div>
     // <div
     //   style={{
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,5 @@
+import PropTypes from 'prop-types';
 import css from './Profile.module.css'
-import data from './user.json'
 
 export const Profile = ({username, tag, location, avatar, stats}) => {
   return (<div className = {css.profile}>
@@ -29,4 +29,16 @@ export const Profile = ({username, tag, location, avatar, stats}) => {
     </li>
   </ul>
 </div>);
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.exact({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
 };
