5b458a5925150b1155e5866d7bc142b21f5269d2
SvetaSaletska <svitlanagalevich@gmail.com>
2023-12-19 23:59:25 +0200

    Creating task 2

---
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import css from './StatisticsItem.module.css'
+
+export const StatisticsItem = ({ label, percentage }) => {
+  const bgc = getRandomHexColor();
+
+  return (
+      <li className={css.item} style={{backgroundColor: `${bgc}`}}>
+          <span className={css.label}>{label}</span>
+          <span className={css.percentage}>{percentage}%</span>
+      </li>
+  );
+}
+
+StatisticsItem.propTypes = {
+  label: PropTypes.string.isRequired,
+  percentage: PropTypes.number.isRequired,
+}
+
+function getRandomHexColor() {
+return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
